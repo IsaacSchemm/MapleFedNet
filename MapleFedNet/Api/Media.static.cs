46524509fe960b1ead959d46280752aea3c9716e
libertyernie <maybeway36@gmail.com>
2019-05-19 17:59:01 -0500

    Attachment upload: add parameter for focus point

--- MapleFedNet/Api/Media.static.cs
+++ MapleFedNet/Api/Media.static.cs
@@ -13,16 +13,22 @@ namespace MapleFedNet.Api
         /// </summary>
         /// <param name="credentials">Mastodon credentials</param>
         /// <param name="file">Media to be uploaded</param>
+        /// <param name="description">A plain-text description of the media for accessibility (max 420 chars)</param>
+        /// <param name="focus">Two floating points used for intelligent cropping</param>
         /// <returns>Returns an <see cref="Attachment" /> that can be used when creating a status</returns>
-        public static async Task<Attachment> Uploading(IMastodonCredentials credentials, byte[] file,
-            string description = null)
+        public static async Task<Attachment> Uploading(
+            IMastodonCredentials credentials,
+            byte[] file,
+            string description = null,
+            (double, double)? focus = null)
         {
             string domain = credentials.Domain;
             string token = credentials.Token;
             return await HttpHelper.Instance.PostAsync<Attachment, HttpContent>(
                 $"{HttpHelper.HTTPS}{domain}{Constants.MediaUploading}", token,
                 (nameof(file), new StreamContent(new MemoryStream(file))),
-                (nameof(description), new StringContent(description)));
+                (nameof(description), new StringContent(description)),
+                (nameof(focus), focus == null ? null : new StringContent($"{focus.Value.Item1},{focus.Value.Item2}")));
         }
     }
 }
